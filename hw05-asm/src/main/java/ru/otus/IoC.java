75770ca3b228d4d41ddd0c247ec90ff40d91e854
roman <RumyantsevRO@gmail.com>
2023-09-27 20:55:22 +0300

    hw05 fix after review

--- hw05-asm/src/main/java/ru/otus/IoC.java
+++ hw05-asm/src/main/java/ru/otus/IoC.java
@@ -6,32 +6,37 @@ import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
+@SuppressWarnings({"unchecked"})
 public class IoC {
     private IoC() {
     }
 
-    static TestLoggingInterface createMyClass() {
-        InvocationHandler handler = new LogInvocationHandler(new TestLoggingImpl());
-        return (TestLoggingInterface) Proxy.newProxyInstance(IoC.class.getClassLoader(), new Class<?>[]{TestLoggingInterface.class}, handler);
+    static <T> T createMyClass(T obj) {
+        if (Objects.isNull(obj)) {
+            throw new IllegalArgumentException("Object must not be null");
+        }
+        Class<T> clazz = (Class<T>) obj.getClass();
+        InvocationHandler handler = new LogInvocationHandler<>(obj);
+        return (T) Proxy.newProxyInstance(IoC.class.getClassLoader(), clazz.getInterfaces(), handler);
     }
 
-    private static class LogInvocationHandler implements InvocationHandler {
-        private final TestLoggingInterface myClass;
+    private static class LogInvocationHandler<T> implements InvocationHandler {
+        private final T object;
         private final List<String> list = new ArrayList<>();
 
-        LogInvocationHandler(TestLoggingInterface myClass) {
-            this.myClass = myClass;
+        LogInvocationHandler(T object) {
+            this.object = object;
             addLogAnnotation();
         }
 
         private void addLogAnnotation() {
-            Method[] methods = myClass.getClass().getMethods();
-            for (var method : methods) {
-                if (method.isAnnotationPresent(Log.class)) {
-                    list.add(method.getName() + Arrays.toString(method.getParameterTypes()));
-                }
-            }
+            Method[] methods = object.getClass().getMethods();
+            list.addAll(Arrays.stream(methods)
+                    .filter(m -> m.isAnnotationPresent(Log.class))
+                    .map(m -> m.getName() + Arrays.toString(m.getParameterTypes()))
+                    .toList());
         }
 
         @Override
@@ -39,7 +44,7 @@ public class IoC {
             if (list.contains(method.getName() + Arrays.toString(method.getParameterTypes()))) {
                 System.out.printf("Executed method: %s, params: %s%n", method.getName(), Arrays.toString(args));
             }
-            return method.invoke(myClass, args);
+            return method.invoke(object, args);
         }
     }
 }
